4f019c79b6f383dec5de7ed021cfb4754a5be80f
kazto kunii <kztkny@gmail.com>
2017-04-11 17:50:46 +0900

    Mod

--- src/main/scala/NoSideEffect.scala
+++ src/main/scala/NoSideEffect.scala
@@ -3,17 +3,20 @@
   */
 object NoSideEffect extends App{
 
+  // 副作用のある関数
   var x:Int = 0
   def sideEffectAdd(y:Int):Int = {
     x += y
     x
   }
 
+  // 副作用のない関数
   def noSideEffectAdd(x: Int) = x + 5
 
   println(s"sideEffectAdd：${sideEffectAdd(1)}")
   println(s"sideEffectAdd：${sideEffectAdd(1)}")
   println(s"sideEffectAdd：${sideEffectAdd(1)}")
+
   println(s"noSideEffectAdd：${noSideEffectAdd(1)}")
   println(s"noSideEffectAdd：${noSideEffectAdd(1)}")
   println(s"noSideEffectAdd：${noSideEffectAdd(1)}")
