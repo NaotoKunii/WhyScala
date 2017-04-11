ae9d6ea5293e098bbc7fb2300056646c2880f75b
kazto kunii <kztkny@gmail.com>
2017-04-11 14:23:52 +0900

    特徴3：副作用を起こさない

---
+++ src/main/scala/NoSideEffect.scala
@@ -0,0 +1,20 @@
+/**
+  * 副作用のない関数
+  */
+object NoSideEffect extends App{
+
+  var x:Int = 0
+  def sideEffectAdd(y:Int):Int = {
+    x += y
+    x
+  }
+
+  def noSideEffectAdd(x: Int) = x + 5
+
+  println(s"sideEffectAdd：${sideEffectAdd(1)}")
+  println(s"sideEffectAdd：${sideEffectAdd(1)}")
+  println(s"sideEffectAdd：${sideEffectAdd(1)}")
+  println(s"noSideEffectAdd：${noSideEffectAdd(1)}")
+  println(s"noSideEffectAdd：${noSideEffectAdd(1)}")
+  println(s"noSideEffectAdd：${noSideEffectAdd(1)}")
+}
